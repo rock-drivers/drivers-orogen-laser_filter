2b51a0d17d3ccf1281eb5249682b2a0b3c83b2c8
Janosch Machowinski <scotch@imoby-Laptop-1.(none)>
2013-11-08 16:21:04 +0100

    updated test script

--- scripts/test_filter.rb
+++ scripts/test_filter.rb
@@ -1,85 +1,72 @@
 #! /usr/bin/env ruby
+#library for displaying data
 require 'vizkit'
-require 'roby/vizkit'
+require 'readline'
+require 'eigen'
+require 'rock/bundle'
 
-folder = ARGV[0]
-
-puts("Folder #{folder}")
-
-
-Orocos.initialize
-
-# setup the environment so that ruby can find the test deployment
-ENV['PKG_CONFIG_PATH'] = "#{File.expand_path("..", File.dirname(__FILE__))}/build:#{ENV['PKG_CONFIG_PATH']}"
+if !ARGV[0]  then 
+    puts "usage: replay.rb log_dir"
+    exit
+end
 
 
-replay = Orocos::Log::Replay.open(folder)
-Vizkit.use_tasks replay.tasks
+#load log file 
+log = Orocos::Log::Replay.open(ARGV[0])
+Orocos::CORBA::max_message_size = 100000000
 
-view3d = Vizkit.default_loader.create_widget('vizkit::Vizkit3DWidget')
+log.transformer_broadcaster.track(false) 
+log.transformer_broadcaster.rename('foo')
+log.name_service.deregister 'transformer_broadcaster'
+#log.name_service.deregister 'local_planner'
 
-laserFrameViz = view3d.createPlugin('RigidBodyStateVisualization')
-laserFrameViz.resetModel(0.5)
+Bundles.initialize
+Bundles.transformer.load_conf(Bundles.find_file('config', 'transforms_scripts.rb'))
 
-laserViz = view3d.createPlugin('LaserScanVisualization')
+Bundles.run 'laser_filter::Task' => 'laser_filter', :valgrind => false, :output => nil do |p|
 
-lBoxViz = view3d.createPlugin('laser_filter', 'BoxVisualization')
-rBoxViz = view3d.createPlugin('laser_filter', 'BoxVisualization')
-
-y_forward_to_x_forward = Eigen::Quaternion.from_angle_axis(Math::PI/2.0, Eigen::Vector3.new(0, 0, 1))
-
-lastLaserFrame = nil
-
-
-Orocos.run "laser_filter_test" do 
+    laser_filter = Bundles::get 'laser_filter'
     
-    laser_filter = Orocos::TaskContext.get('laser_filter')
-    
-    laser_filter.maskedNeighbours = 3;
-    laser_filter.minIncline = 0.2
-    laser_filter.maxIncline = Math::PI - 0.2
-    
-    leftBox = Types::LaserFilter::Box.new
-    leftBox.downLeft = Eigen::Vector3.new(0.2, -0.25, -0.2)
-    leftBox.upRight = Eigen::Vector3.new(0.4, 0.25, 0.5)
- 
-    rightBox = Types::LaserFilter::Box.new
-    rightBox.downLeft = Eigen::Vector3.new(-0.4, -0.25, -0.2)
-    rightBox.upRight = Eigen::Vector3.new(-0.20, 0.25, 0.5)
-    
-    
-    boxes = laser_filter.filterBoxes;
-    boxes << leftBox
-    boxes << rightBox
-    
-    laser_filter.filterBoxes = boxes
+    laser_filter.apply_conf(['default', 'laser_back'], true)
 
-    lBoxViz.updateData(leftBox)
-    rBoxViz.updateData(rightBox)
-    
-    replay.hokuyo.scans.connect_to laser_filter.scan_samples, :type => :buffer, :size => 100
+    hokuyo_back = Bundles::get 'hokuyo_back'
 
-    Vizkit.connect_port_to 'laser_filter', 'filtered_scans', :auto_reconnect => true, :pull => false, :update_frequency => 33 do |sample, name|
-	if(laserFrameViz)
-	    laserViz.updateData(lastLaserFrame)
-	    laserViz.updateData(sample)
-	end
-	sample
-    end    
+    hokuyo_back.scans.connect_to(laser_filter.scan_samples, :type => :buffer, :size => 100)
 
-    Vizkit.connect_port_to 'laser_filter', 'debug_laser_frame', :auto_reconnect => true, :pull => false, :update_frequency => 33 do |sample, name|
-	lastLaserFrame = sample;
-	lastLaserFrame.orientation = lastLaserFrame.orientation * y_forward_to_x_forward
-	sample
-    end    
-    
-    replay.dynamixel.lowerDynamixel2UpperDynamixel.connect_to laser_filter.dynamic_transformations, :type => :buffer, :size => 100
-    
+    Bundles.transformer.setup( laser_filter )
+
     laser_filter.configure
     laser_filter.start
-    
-    view3d.show
 
-    Vizkit.control replay
-    Vizkit.exec
+    Vizkit.control log
+
+    boxes = laser_filter.filterBoxes
+    
+    pp boxes
+    
+    #laserFrameViz = view3d.createPlugin('RigidBodyStateVisualization')
+    #laserFrameViz.resetModel(0.5)
+
+    #laserViz = view3d.createPlugin('LaserScanVisualization')
+
+    lBoxViz = Vizkit.default_loader.BoxVisualization
+    rBoxViz = Vizkit.default_loader.BoxVisualization
+    #Vizkit.display laser_filter.filterBoxes
+    lBoxViz.updateData(boxes[0])
+    rBoxViz.updateData(boxes[1])
+
+    laserViz =  Vizkit.display laser_filter.filtered_scans , :widget => Vizkit.default_loader.LaserScanVisualization
+
+    Vizkit.connect_port_to 'laser_filter', 'debug_laser_frame', :auto_reconnect => true, :pull => false, :update_frequency => 33 do |sample, name|
+        laserViz.updateData(sample)
+        sample
+    end    
+
+    
+    
+#     Vizkit.display heading_calc.heading_debug_deg
+
+    Vizkit.exec() 
 end
+
+
